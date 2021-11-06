6970993e9f6c85ce9f16de75c17704f4bacba2a4
DOUGBOWMER <dougbowmer3@gmail.com>
2021-11-06 16:00:44 -0400

    downrgade node version 14.17

--- firebase/index.js
+++ firebase/index.js
@@ -1,12 +1,9 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+// Need to update below config
+const config = {
   apiKey: "AIzaSyAaZeR_zKr9rll3P6sTZXg9DAH5agUOCmQ",
   authDomain: "fly-veggies.firebaseapp.com",
   projectId: "fly-veggies",
@@ -15,7 +12,11 @@ const firebaseConfig = {
   appId: "1:487426968637:web:ee5c5cec7992672a662e54",
   measurementId: "G-L44508VYG2"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+if (!firebase.apps.length) {
+    firebase.initializeApp(config);
+}
+const auth = firebase.auth();
+export {
+    auth,
+    firebase
+};
