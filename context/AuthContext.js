515d032c3631a2f194836362134388135c600651
Vyacheslav Nosal <firstArachne0116@gmail.com>
2021-11-12 05:11:36 +0900

    Signup complete

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -1,8 +1,6 @@
 import React, { useContext, useState, useEffect } from "react"
-import { auth, firebase } from "../firebase/index"
+import { auth } from "../firebase/index"
 import "react-toastify/dist/ReactToastify.min.css";
-import Router from 'next/router';
-import Alert from '../components/Alert/Alert'
 
 const AuthContext = React.createContext()
 
@@ -13,26 +11,9 @@ export function useAuth() {
 export function AuthProvider({ children }) {
   const [currentUser, setCurrentUser] = useState()
   const [loading, setLoading] = useState(true)
-  const db = firebase.firestore();
 
-  function signup(email, password, fname, lname) {
+  function signup(email, password) {
     return auth.createUserWithEmailAndPassword(email, password)
-            .then((userCredential) => {
-              Router.push('/login');
-              Alert('success', 'Successfully Registered!')
-              auth.signOut();
-            })
-            .catch(error => {
-              console.log(error.code)
-              switch(error.code) {
-                case 'auth/email-already-in-use': 
-                  Alert('error', 'Email Already Exist')
-                  break
-                case 'auth/weak-password':
-                  Alert('warning', 'Password is too weak')
-                  break
-              } 
-            });
   }
 
   function login(email, password) {
--- firebase/index.js
+++ firebase/index.js
@@ -4,13 +4,13 @@ import 'firebase/firestore';
 
 // Need to update below config
 const config = {
-  apiKey: "AIzaSyDv_hX8L_m3ZQHAktg-sh_lDGSBnjHLIfs",
-  authDomain: "adddatatest-c8f62.firebaseapp.com",
-  projectId: "adddatatest-c8f62",
-  storageBucket: "adddatatest-c8f62.appspot.com",
-  messagingSenderId: "781440124235",
-  appId: "1:781440124235:web:56ccc1cd6ad112a27dd4ce",
-  measurementId: "G-LSQCW8SYCF"
+  apiKey: "AIzaSyAaZeR_zKr9rll3P6sTZXg9DAH5agUOCmQ",
+  authDomain: "fly-veggies.firebaseapp.com",
+  projectId: "fly-veggies",
+  storageBucket: "fly-veggies.appspot.com",
+  messagingSenderId: "487426968637",
+  appId: "1:487426968637:web:ee5c5cec7992672a662e54",
+  measurementId: "G-L44508VYG2"
 };
 if (!firebase.apps.length) {
     firebase.initializeApp(config);
